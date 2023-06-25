c8620fa0c3fdf5dc524920b45f92a51f72aacb1b
taku-zfof <taku.uchida.zfof@gmail.com>
2023-06-26 06:52:09 +0900

    [add]pitagora2.rb

--- pitagora.rb
+++ pitagora.rb
@@ -1,10 +1,10 @@
-x = 6
-y = 9
+x = 3
+y = 4
 
 henA = x
 henB = y
 
-syahenn = x^2 + y^2 
+syahenn = x**2 + y**2 
 
 syahen = Math.sqrt(syahenn)
-p syahenn
+p syahen
---
+++ pitagora2.rb
@@ -0,0 +1,9 @@
+x = 4
+y = 5
+
+henA = x
+syahen = y
+
+henBB = syahen**2 - henA**2
+henB = Math.sqrt(henBB)
+p henB 
