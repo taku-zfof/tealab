e295c8f9677273c80266f1cba872a6d96eb26267
taku-zfof <taku.uchida.zfof@gmail.com>
2023-07-01 23:59:59 +0900

    [add] forfrontier課題

---
+++ forfrontier.rb
@@ -0,0 +1,26 @@
+【Java】
+https://paiza.io/ja/projects/new?language=java
+【Python】
+https://paiza.io/ja/projects/new?language=python3
+【Ruby】
+https://paiza.io/ja/projects/new?language=ruby
+【Swift】
+https://paiza.io/ja/projects/new?language=swift
+【PHP】
+https://paiza.io/ja/projects/new?language=php
+
+九九の計算式と結果を9の段から1の段を降順に出力してください
+ただし、計算結果が27については出力しない
+
+【出力結果イメージ】
+9 × 1 =  9
+9 × 2 = 18
+・・・
+9 × 9 = 81
+・・・
+8 × 1 =  8
+・・・
+8 × 9 = 72
+・・・
+1 × 8 =  8
+1 × 9 =  9
